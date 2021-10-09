6e554c620423ad571a0c48d81a458202e7dfd8b5
Байел Зарлыков <baielzarlykov@MacBook-Air-baiel.local>
2021-10-09 20:21:22 +0600

    it-cinema

--- djangocinema/urls.py
+++ djangocinema/urls.py
@@ -19,6 +19,8 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
+from movie.views import MovieListView
+
 schema_view = get_schema_view(
     openapi.Info(
         title='Cinema',
@@ -30,9 +32,12 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('api/v1/movie', MovieListView.as_view()),
+    # # path('api/v1/auth', include('djoser.urls')),
+    path('api/v1/auth', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
     path('admin/', admin.site.urls),
     path('', include('movie.urls')),
-    path('api/v1/', include('djoser.urls')),
     path(r'^(?P<format>\.json|\/.yaml)/$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
--- movie/urls.py
+++ movie/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('api/v1/movie', MovieListView.as_view()),
-    path('api/v1/auth', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.authtoken')),
+    # path('api/v1/movie', MovieListView.as_view()),
+    # # path('api/v1/auth', include('djoser.urls')),
+    # path('auth', include('djoser.urls')),
     path('movie/<int:pk>', MovieDetail.as_view()),
     path('review/', ReviewCreateView.as_view()),
     path('create/movie/', CreateMovieView.as_view())
